41115a34e699e99134f56e640423d16f4d73de4e
Stephany <stephanieoguocha@gmail.com>
2022-10-08 16:53:44 +0100

    Environmet Variables

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require ("mongoose");
@@ -20,8 +21,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Thisismylittlesecret.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
